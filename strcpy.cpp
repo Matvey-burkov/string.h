3a16b7c49f21a87b020fc340b6b9fa97b8be103c
Matvey-burkov <112969099+Matvey-burkov@users.noreply.github.com>
2023-11-14 12:54:04 +0700

    Update strcpy.cpp

--- strcpy.cpp
+++ strcpy.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     const char* source = "Пример строки для копирования";
-    char destination[100]; // Здесь указываем максимальную длину строки
+    char destination[100]; 
 
     int i = 0;
     while (source[i] != '\0') {
@@ -10,7 +10,7 @@ int main() {
         i++;
     }
 
-    destination[i] = '\0'; // Обязательно завершаем строку нулевым символом
+    destination[i] = '\0'; 
 
     std::cout << "Скопированная строка: " << destination << std::endl;
 
